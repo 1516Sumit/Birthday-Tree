0079258ec7e0659ad780c1226ede8b0083c51651
1516Sumit <sumitsharma781425@gmail.com>
2024-07-09 21:12:38 +0530

    hindi

--- script.js
+++ script.js
@@ -5,5 +5,21 @@ document.addEventListener('DOMContentLoaded', function() {
     toggleButton.addEventListener('click', function() {
         navbar.classList.toggle('hidden');
     });
+
+    const translateButton = document.getElementById('google_element');
+    const translateElement = document.getElementById('google_translate_element');
+    
+    translateButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        if (translateElement.style.display === 'none' || translateElement.style.display === '') {
+            translateElement.style.display = 'block';
+            translateElement.style.top = `${translateButton.offsetTop + translateButton.offsetHeight}px`;
+            translateElement.style.left = `${translateButton.offsetLeft}px`;
+            new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+        } else {
+            translateElement.style.display = 'none';
+        }
+    });
+
     AOS.init();
 });
